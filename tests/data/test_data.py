e9971bafbc28b69628fd4b296446178f8a30c363
pparul <parul.pandey85@gmail.com>
2023-10-26 17:22:50 -0700

    fix test.py issue

--- tests/data/test_data.py
+++ tests/data/test_data.py
@@ -3,10 +3,12 @@ def test_dataset(df):
     column_list = ["id", "created_on", "title", "description", "tag"]
     df.expect_table_columns_to_match_ordered_list(column_list=column_list)
     # # schema adherence
-    # tags = ["computer-vision", "natural-language-processing", "mlops", "other"]
+    # tags = ["computer-vision", "natural-language-processing",
+    # "mlops", "other"]
     # df.expect_column_values_to_be_in_set(column="tag", value_set=tags)
     # # # expected labels
-    # # df.expect_compound_columns_to_be_unique(column_list=["title", "description"])
+    # # df.expect_compound_columns_to_be_unique(column_list=
+    # ["title", "description"])
     # # # data leaks
     # # df.expect_column_values_to_not_be_null(column="tag")
     # # # missing values
@@ -16,6 +18,8 @@ def test_dataset(df):
     # # # type adherence
 
     # Expectation suite
-    expectation_suite = df.get_expectation_suite(discard_failed_expectations=False)
-    results = df.validate(expectation_suite=expectation_suite, only_return_failures=True).to_json_dict()
+    expectation_suite = df.get_expectation_suite(\
+    discard_failed_expectations=False)
+    results = df.validate(expectation_suite=expectation_suite,\
+    only_return_failures=True).to_json_dict()
     assert results["success"]
